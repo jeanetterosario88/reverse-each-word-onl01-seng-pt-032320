69e90e773b8630aad0aa252f78483c0a99e7d9ec
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-01 02:59:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,13 +7,11 @@ def reverse_each_word(sentence)
   reversed.join(" ")
 end
 
-rdef reverse_each_word(sentence)
+def reverse_each_word(sentence)
   reversed=[]
-  array=sentence.split
+  array=sentence.collect
   array.each do |word|
     reversed<<"#{word.reverse}"
   end
   reversed.join(" ")
 end
-
-reverse_each_word("I am a girl.")
